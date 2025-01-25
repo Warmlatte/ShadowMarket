545c96e5f23667c6c43c7b946aab0c905ba5c0fc
Zhi-Yan,Haung <w0975582420@gmail.com>
2025-01-26 01:31:57 +0800

    feat: complete NoticeSection layout implementation

--- frontend/src/components/NoticeSection.vue
+++ frontend/src/components/NoticeSection.vue
@@ -1,7 +1,130 @@
 <script setup></script>
 
 <template>
-  <div class="p-5">Notice</div>
+  <div class="m-10">
+    <div class="mockup-window bg-neutral border">
+      <div class="bg-base-200 flex flex-col items-center px-6 pt-6 pb-8 space-y-4">
+        <!-- small -->
+        <div class="md:hidden lg:hidden">
+          <h2 class="text-xl font-bold">🛒 選購須知</h2>
+          <h2 class="text-lg font-semibold">冒險者的購物指南</h2>
+        </div>
+        <!-- large -->
+        <h2 class="hidden md:block lg:block text-2xl font-bold mb-2">
+          🛒 選購須知：冒險者的購物指南
+        </h2>
+
+        <!-- 條列內容 -->
+        <div class="space-y-4 border-t-2 border-dashed pt-4">
+          <!-- small -->
+          <div class="md:hidden lg:hidden text-md">
+            <p class="mb-1">
+              <strong>📜 購買一般物品</strong>
+            </p>
+            <p>
+              請移駕至
+              <a
+                class="link link-hover"
+                href="https://5eclone.pingstudio.tw/items.html#%e4%b8%80%e7%9b%92%e7%b6%a0%e6%9d%be%e7%9f%b3%e5%88%b6%e7%9a%84%e5%8b%95%e7%89%a9%e9%9b%95%e5%83%8f_dmg"
+                target="_blank"
+              >
+                5etools中文版
+              </a>
+            </p>
+          </div>
+          <!-- large -->
+          <div class="hidden md:block lg:block">
+            <p class="text-lg">
+              <strong>📜 購買一般物品：</strong>
+              請移駕至
+              <a
+                class="link link-hover"
+                href="https://5eclone.pingstudio.tw/items.html#%e4%b8%80%e7%9b%92%e7%b6%a0%e6%9d%be%e7%9f%b3%e5%88%b6%e7%9a%84%e5%8b%95%e7%89%a9%e9%9b%95%e5%83%8f_dmg"
+                target="_blank"
+              >
+                5etools中文版
+              </a>
+            </p>
+          </div>
+
+          <!-- small -->
+          <div class="md:hidden lg:hidden text-md text-red-500">
+            <p class="mb-1">
+              <strong>🚫 禁止購買爆炸物</strong>
+            </p>
+            <p>無法保證交易過後的安全性！</p>
+          </div>
+          <!-- large -->
+          <div class="hidden md:block lg:block">
+            <p class="text-lg text-red-500">
+              <strong>🚫 禁止購買爆炸物：</strong>
+              無法保證交易過後的安全性！
+            </p>
+          </div>
+
+          <!-- small -->
+          <div class="md:hidden lg:hidden text-md text-yellow-500">
+            <p class="mb-1">
+              <strong>⚖️ 禁止購買超過負重的物品</strong>
+            </p>
+            <p>冒險者的裝備要輕便！</p>
+          </div>
+          <!-- large -->
+          <div class="hidden md:block lg:block">
+            <p class="text-lg text-yellow-500">
+              <strong>⚖️ 禁止購買超過負重的物品：</strong>
+              冒險者的裝備要輕便！
+            </p>
+          </div>
+
+          <!-- small -->
+          <div class="md:hidden lg:hidden text-md text-green-500">
+            <p class="mb-1">
+              <strong>🚗 禁止購買載具</strong>
+            </p>
+            <p>本市集不販售馬車或船隻。</p>
+          </div>
+          <!-- large -->
+          <div class="hidden md:block lg:block">
+            <p class="text-lg text-green-500">
+              <strong>🚗 禁止購買載具：</strong>
+              本市集不販售馬車或船隻。
+            </p>
+          </div>
+
+          <!-- small -->
+          <div class="md:hidden lg:hidden text-md text-purple-500">
+            <p class="mb-1">
+              <strong>🦄 禁止購買生物</strong>
+            </p>
+            <p>冒險者須遵守保護魔法生物法規！</p>
+          </div>
+          <!-- large -->
+          <div class="hidden md:block lg:block">
+            <p class="text-lg text-purple-500">
+              <strong>🦄 禁止購買生物：</strong>
+              冒險者須遵守保護魔法生物法規！
+            </p>
+          </div>
+
+          <!-- small -->
+          <div class="md:hidden lg:hidden text-md text-gray-500">
+            <p class="mb-1">
+              <strong>❌ 禁止購買其他未列物品</strong>
+            </p>
+            <p>僅限購買「魔法道具分頁」的物品。</p>
+          </div>
+          <!-- large -->
+          <div class="hidden md:block lg:block">
+            <p class="text-lg text-gray-500">
+              <strong>❌ 禁止購買其他未列物品</strong>
+              僅限購買「魔法道具分頁」的物品。
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
