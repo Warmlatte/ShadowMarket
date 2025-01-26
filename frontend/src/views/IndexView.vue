a721f01d69209646ae32d9fded447588a4a3ae48
Zhi-Yan,Haung <w0975582420@gmail.com>
2025-01-26 22:19:02 +0800

    chore: adjust product-related layout styling and positioning

--- frontend/src/views/IndexView.vue
+++ frontend/src/views/IndexView.vue
@@ -1,14 +1,14 @@
 <script setup>
 import Navbar from '@/components/NavbarComponent.vue'
 import Notice from '@/components/NoticeSection.vue'
-import MarketPlace from '@/components/MarketplaceSection.vue'
+import Item from '@/components/Items/ItemModal.vue'
 import Footer from '@/components/FooterComponent.vue'
 </script>
 
 <template>
   <Navbar />
   <Notice />
-  <MarketPlace />
+  <Item />
   <Footer />
 </template>
 
