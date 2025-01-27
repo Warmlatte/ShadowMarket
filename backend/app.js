141382e6bd3190ceedffab1df78eb21c6e99d1f3
Zhi-Yan,Haung <w0975582420@gmail.com>
2025-01-28 00:12:07 +0800

    chore: add error-handling middleware for Prisma

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,19 @@
 import express from "express";
 import cors from "cors";
-// import errorMiddleware from ""
+import errorMiddleware from "./src/middlewares/errorMiddleware.js";
 import Items from "./src/routes/itemRoutes.js";
 
 const app = express();
 
-// middleware
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-// routes
+// Routes
 app.use("/items", Items);
 
-// error
-// app.use(errorMiddleware)
+// Error Handler
+app.use(errorMiddleware);
 
 export { app };
