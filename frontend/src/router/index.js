a79077d32ac99b60ef4eb06192dcfa34b07f9bea
Zhi-Yan,Haung <w0975582420@gmail.com>
2025-01-27 13:51:24 +0800

    feat: complete basic layout for edit page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import IndexView from '../views/IndexView.vue'
+import EditItemView from '../views/EditItemView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: IndexView,
+    },
+    {
+      path: '/management',
+      name: 'management',
+      component: EditItemView,
     },
   ],
 })
