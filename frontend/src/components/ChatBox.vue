ec47be9af66404993e664567f6606be437f09930
Zhi-Yan,Haung <w0975582420@gmail.com>
2025-01-31 18:01:17 +0800

    style: update button styling

--- frontend/src/components/ChatBox.vue
+++ frontend/src/components/ChatBox.vue
@@ -58,12 +58,35 @@ const sendMessage = async () => {
 </script>
 
 <template>
-  <button
-    @click="toggleChat"
-    class="fixed bottom-40 right-6 btn btn-outline z-10 opacity-20 w h-[2rem]"
-  >
-    影界入口 ░▒▓█
-  </button>
+  <div class="fixed bottom-10 right-4 z-10">
+    <button
+      @click="toggleChat"
+      class="group relative inline-flex h-12 w-12 items-center justify-center overflow-hidden rounded-full bg-base-200 font-medium transition-all duration-300 hover:w-32"
+    >
+      <div
+        class="inline-flex whitespace-nowrap opacity-0 transition-all duration-200 group-hover:-translate-x-3 group-hover:opacity-100"
+      >
+        影界入口
+      </div>
+      <div class="absolute right-3.5">
+        <svg
+          width="15"
+          height="15"
+          viewBox="0 0 15 15"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5"
+        >
+          <path
+            d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
+            fill="currentColor"
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </div>
+    </button>
+  </div>
 
   <div v-if="isOpen" class="modal modal-open">
     <div class="modal-box relative max-w-3xl shadow-xl">
@@ -102,7 +125,21 @@ const sendMessage = async () => {
           class="input input-bordered flex-1 rounded-l-md"
           placeholder="輸入你的問題..."
         />
-        <button @click="sendMessage" class="btn rounded-r-md">發送</button>
+        <button
+          @click="sendMessage"
+          class="hidden md:block group relative h-12 overflow-hidden rounded-md bg-base-300 px-6 transition"
+        >
+          <span>發送</span>
+          <div
+            class="absolute inset-0 h-full w-0 bg-white/10 transition-[width] group-hover:w-full"
+          ></div>
+        </button>
+        <button
+          @click="sendMessage"
+          class="md:hidden group relative inline-flex h-12 items-center justify-center overflow-hidden rounded-md border border-neutral-500 bg-transparent px-6 font-medium transition-all [box-shadow:0px_4px_1px_#a3a3a3] active:translate-y-[2px] active:shadow-none"
+        >
+          發送
+        </button>
       </div>
     </div>
   </div>
